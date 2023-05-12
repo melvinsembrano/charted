aaccb2455b4033aade56ed73884fb3bc7a978679
Melvin Sembrano <melv@hey.com>
2023-05-12 14:01:06 +1000

    wip: charting

---
+++ src/charted/pie.rs
@@ -0,0 +1,6 @@
+pub fn generate(input_file: String, output_path: String) {
+    println!(
+        "Generating pie chart from {} to {}",
+        input_file, output_path
+    );
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+mod charted;
+
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value = "dot")]
+    #[arg(short, long, default_value = "pie")]
     graphtype: String,
 
     #[arg(short, long)]
@@ -13,9 +15,21 @@ struct Args {
     output: String,
 }
 
+fn generate_chart(args: Args) -> Result<(), Box<dyn std::error::Error>> {
+    match args.graphtype.to_lowercase().as_str() {
+        "pie" => charted::pie::generate(args.input, args.output),
+        _ => {
+            println!("Unknown graph type {}", args.graphtype)
+        }
+    }
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    println!("{:?}", args);
+    // println!("{:?}", args);
+
+    generate_chart(args)?;
 
     Ok(())
 }
