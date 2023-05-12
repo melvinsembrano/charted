b911c798098ae91bf1eb94b6b8630a73cacc13e7
Melvin Sembrano <melv@hey.com>
2023-05-12 12:17:14 +1000

    argument handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, default_value = "dot")]
+    graphtype: String,
+
+    #[arg(short, long)]
+    input: String,
+
+    #[arg(short, long)]
+    output: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    println!("{:?}", args);
+
+    Ok(())
 }
